8f0a59df1bd0bf801230d2dd0877a112779ea805
Waqar Detho <waqar.detho@gmail.com>
2023-11-20 17:37:39 +0100

    Update controller

--- src/main/java/com/frankmoley/lil/learningspring/web/GuestController.java
+++ src/main/java/com/frankmoley/lil/learningspring/web/GuestController.java
@@ -1,13 +1,18 @@
 package com.frankmoley.lil.learningspring.web;
 
 import com.frankmoley.lil.learningspring.business.ReservationService;
+import com.frankmoley.lil.learningspring.data.Guest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping("/guests")
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/api/")
 public class GuestController {
     private final ReservationService reservationService;
 
@@ -15,10 +20,10 @@ public class GuestController {
         this.reservationService = reservationService;
     }
 
-    @RequestMapping(method = RequestMethod.GET)
-    public String getGuests(Model model){
-        model.addAttribute("guests", this.reservationService.getHotelGuests());
-        return "hotel-guests";
+    @GetMapping("/guests")
+    public Collection<Guest> getGuests() {
+        return this.reservationService.getHotelGuests();
+
     }
 }
 
